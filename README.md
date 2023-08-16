b97569934b6109479def2d50e085650d80b58b9e
Azat Bariev <justlumajustmail@gmail.com>
2023-08-16 14:04:45 +0300

    update app and Readme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ NodeJs and RabbitMQ test task
 How to run:
 
 Instruction for Ubuntu OS
+0. Clone repo: `git clone https://github.com/Ziucay/test-task-nodejs.git`
 1. Install docker: https://docs.docker.com/engine/install/ubuntu/
 2. Install docker-compose: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-20-04
 3. 
--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http");
 http.createServer(function(request,response){
-
-    response.end("Hello NodeJS changed!");
+    response.setHeader("Content-Type", "text/plain; charset=utf-8;");
+    response.write("Hello NodeJS changed!")
+    response.end();
 
 }).listen(3000, "127.0.0.1",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
