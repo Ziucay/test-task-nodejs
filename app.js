2ccf45a3fdb14b50c9318df3f77939e26476fd21
Azat Bariev <justlumajustmail@gmail.com>
2023-08-16 14:10:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ http.createServer(function(request,response){
     response.write("Hello NodeJS changed!")
     response.end();
 
-}).listen(3000, "127.0.0.1",function(){
+}).listen(3000, "0.0.0.0",function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
 });
